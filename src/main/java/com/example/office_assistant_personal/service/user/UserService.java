918f14606757c5d405d62bb93e15bca8d84dccef
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2023-02-21 13:09:20 +0600

    Add files via upload

---
+++ src/main/java/com/example/office_assistant_personal/service/user/UserService.java
@@ -0,0 +1,28 @@
+package com.example.office_assistant_personal.service.user;
+
+import com.example.office_assistant_personal.DTO.UserDTO;
+import com.example.office_assistant_personal.entity.user.User;
+import com.example.office_assistant_personal.exception.ApiNotAcceptableException;
+import com.example.office_assistant_personal.exception.ApiSystemException;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserDTO createAdmin (UserDTO userDTO) throws ApiSystemException, ApiNotAcceptableException;
+
+    UserDTO createEmployeeByAdmin (UserDTO userDTO) throws ApiSystemException, ApiNotAcceptableException;
+
+    void delete (Long id) throws ApiSystemException;
+
+    List<UserDTO> getAll ();
+
+    UserDTO getById (Long id) throws ApiSystemException;
+
+    User updateUser(User user) throws ApiSystemException, ApiNotAcceptableException;
+
+    UserDTO update (UserDTO userDTO) throws ApiSystemException;
+
+    UserDTO resetPassword (Long id, String password) throws ApiSystemException;
+
+}
